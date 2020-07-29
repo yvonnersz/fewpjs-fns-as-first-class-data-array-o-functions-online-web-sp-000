9d9b216e5217dfd5ca65b5e81609cb484967efc8
Yvonne M Pham <yvonnersz@gmail.com>
2020-07-29 02:41:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 function wakeDog() {
   console.log("Wake Byron the poodle");
 }
+
+function leaseDog() {
+  
+}
+
+function walkDog() {}
+
+function throwFrisbee() {}
+
+function walkHome() {}
+
+function unleashDog() {}
