032889c8ce10e1dc078abfd4ee5bcd8bdf8347ed
Yvonne M Pham <yvonnersz@gmail.com>
2020-07-29 02:44:13 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,23 @@
+function wakeDog(name, breed) {
+  console.log(`Wake ${name} the ${breed}`);
+}
+
+function leaseDog() {
+  console.log("Leash Byron the poodle");
+}
+
+function walkToPark() {
+  console.log("Walk to the park with Byron the poodle");
+}
+
+function throwFrisbee() {
+  console.log("Throw the frisbee for Byron the poodle");
+}
+
+function walkHome() {
+  console.log("Walk home with Byron the poodle");
+}
+
+function unleashDog() {
+  console.log("Unleash Byron the poodle");
+}
