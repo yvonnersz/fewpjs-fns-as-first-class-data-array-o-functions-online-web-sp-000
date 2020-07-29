d12c23c3cb9537e17c384cd1006ac4b9ac8b6e1b
Yvonne M Pham <yvonnersz@gmail.com>
2020-07-29 03:03:43 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,40 @@
+function wakeDog(name, breed) {
+  return `Wake ${name} the ${breed}`
+}
+
+function leashDog(name, breed) {
+  return `Leash ${name} the ${breed}`
+}
+
+function walkToPark(name, breed) {
+  return `Walk to the park with ${name} the ${breed}`
+}
+
+function throwFrisbee(name, breed) {
+  return `Throw the frisbee for ${name} the ${breed}`
+}
+
+function walkHome(name, breed) {
+  return `Walk home with ${name} the ${breed}`
+}
+
+function unleashDog(name, breed) {
+  return `Unleash ${name} the ${breed}`
+}
+
+let routine = [
+  wakeDog,
+  leashDog,
+  walkToPark,
+  throwFrisbee,
+  walkHome,
+  unleashDog
+];
+
+function exerciseDog(dogName, dogBreed) {
+  let routineMessages = [];
+  for (let i=0; i<routine.length; i++) {
+    routineMessages.push(routine[i](dogName, dogBreed));
+  }
+  return routineMessages;
+}
