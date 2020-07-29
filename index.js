75d32fb4fc27dc4509b0c1ae337519e36cf58ff1
Yvonne M Pham <yvonnersz@gmail.com>
2020-07-29 02:48:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,21 +3,21 @@ function wakeDog(name, breed) {
 }
 
 function leashDog(name, breed) {
-  return `Leash Byron the poodle`
+  return `Leash ${name} the poodle`
 }
 
 function walkToPark(name, breed) {
-  return `Walk to the park with Byron the poodle`
+  return `Walk to the park with ${name} the poodle`
 }
 
 function throwFrisbee(name, breed) {
-  return `Throw the frisbee for Byron the poodle`
+  return `Throw the frisbee for ${name} the poodle`
 }
 
 function walkHome(name, breed) {
-  return `Walk home with Byron the poodle`
+  return `Walk home with ${name} the poodle`
 }
 
 function unleashDog(name, breed) {
-  return `Unleash Byron the poodle`
+  return `Unleash ${name} the poodle`
 }
