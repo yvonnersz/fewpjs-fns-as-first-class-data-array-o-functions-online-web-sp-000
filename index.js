aeb27ac6bb476f8765208841e03da4699fbb7793
Yvonne M Pham <yvonnersz@gmail.com>
2020-07-29 02:46:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-function wakeDog(dogName, dogBreed) {
-  return `Wake ${dogName} the ${dogBreed}`;
+function wakeDog(name, breed) {
+  console.log(`Wake ${name} the ${breed}`);
+}
+
+function leashDog(name, breed) {
+  console.log("Leash Byron the poodle");
+}
+
+function walkToPark(name, breed) {
+  console.log("Walk to the park with Byron the poodle");
+}
+
+function throwFrisbee(name, breed) {
+  console.log("Throw the frisbee for Byron the poodle");
+}
+
+function walkHome(name, breed) {
+  console.log("Walk home with Byron the poodle");
+}
+
+function unleashDog(name, breed) {
+  console.log("Unleash Byron the poodle");
 }
